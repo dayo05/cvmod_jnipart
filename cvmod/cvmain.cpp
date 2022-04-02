f337295bd600262fda286f0cf8977ca19ee066e6
dayo05 <dayo05@daum.net>
2022-04-02 19:55:12 +0900

    Working fine

--- cvmod/cvmain.cpp
+++ cvmod/cvmain.cpp
@@ -1,11 +1,11 @@
 #include "cvmain.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 using namespace cv;
 
 static Mat image;
-static void* a;
-static std::vector<uchar> encodedImage = std::vector<uchar>();
-static std::vector<int> param = std::vector<int>(2);
+static VideoCapture video;
 
 JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_loadImage(JNIEnv* env, jobject obj, jstring jname)
 {
@@ -16,6 +16,32 @@ JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_loadImage(JNIEnv* env, job
 	else cvtColor(image, image, COLOR_BGRA2RGBA);
 	
 	env->ReleaseStringUTFChars(jname, name);
+	return (long)image.data;
+}
+
+JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_loadVideo(JNIEnv* env, jobject obj, jstring jname)
+{
+	auto name = env->GetStringUTFChars(jname, nullptr);
+	video = VideoCapture(name);
+	if(!video.isOpened())
+	{
+		printf("Video is not opened");
+		return (long)nullptr;
+	}
+	video >> image;
+	if (image.channels() != 4)
+		cvtColor(image, image, COLOR_BGR2RGBA);
+	else cvtColor(image, image, COLOR_BGRA2RGBA);
+	env->ReleaseStringUTFChars(jname, name);
+	return (long)image.data;
+}
+
+JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_nextFrame(JNIEnv* env, jobject obj)
+{
+	video >> image;
+	if (image.channels() != 4)
+		cvtColor(image, image, COLOR_BGR2RGBA);
+	else cvtColor(image, image, COLOR_BGRA2RGBA);
 	return (long)image.data;
 }
 
@@ -34,15 +60,12 @@ JNIEXPORT jint Java_me_ddayo_cvmod_client_gui_CvUtil_getImageChannels(JNIEnv* en
 	return image.channels();
 }
 
-JNIEXPORT jobject Java_me_ddayo_cvmod_client_gui_CvUtil_getImageByteBuffer(JNIEnv* env, jobject obj)
+JNIEXPORT jboolean Java_me_ddayo_cvmod_client_gui_CvUtil_isVideoFinished(JNIEnv* env, jobject obj)
 {
-	int imsize = image.cols * image.rows * image.channels();
-	encodedImage.resize(imsize);
-	imencode(".bmp", image, encodedImage);
-	env->NewDirectByteBuffer(&encodedImage[0], imsize);
+	return image.empty();
 }
 
-JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_getBmpImage(JNIEnv* env, jobject obj)
+JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_getMatrix(JNIEnv* env, jobject obj)
 {
 	return (long)image.data;
 }
--- cvmod/cvmain.h
+++ cvmod/cvmain.h
@@ -3,17 +3,16 @@
 
 #pragma warning(disable:4996)
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <jni.h>
-#include <vector>
 
 extern "C" {
 	JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_loadImage(JNIEnv*, jobject, jstring);
+	JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_loadVideo(JNIEnv* env, jobject obj, jstring jname);
+	JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_nextFrame(JNIEnv* env, jobject obj);
 	JNIEXPORT jint Java_me_ddayo_cvmod_client_gui_CvUtil_getImageWidth(JNIEnv*, jobject);
 	JNIEXPORT jint Java_me_ddayo_cvmod_client_gui_CvUtil_getImageHeight(JNIEnv*, jobject);
 	JNIEXPORT jint Java_me_ddayo_cvmod_client_gui_CvUtil_getImageChannels(JNIEnv*, jobject);
-	JNIEXPORT jobject Java_me_ddayo_cvmod_client_gui_CvUtil_getImageByteBuffer(JNIEnv* env, jobject obj);
-	JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_getBmpImage(JNIEnv*, jobject);
+	JNIEXPORT jboolean Java_me_ddayo_cvmod_client_gui_CvUtil_isVideoFinished(JNIEnv* env, jobject obj);
+	JNIEXPORT jlong Java_me_ddayo_cvmod_client_gui_CvUtil_getMatrix(JNIEnv*, jobject);
 }
 #endif
